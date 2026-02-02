f442e892cc244908ec81b97f52e789d39d98f180
KOUSHIK C M <koushikmanohar83@gmail.com>
2026-02-02 16:18:39 +0530

    Create ripple_carry_adder.v

---
+++ COMBINATIONAL CIRCUITS/ADDER/RIPPLE CARRY ADDER/DATA FLOW MODELLING/USING CONCATENATION OPERATOR/ripple_carry_adder.v
@@ -0,0 +1,7 @@
+module ripple_carry_adder(input[3:0]a,
+                          input[3:0]b,
+                          input cin,
+                          output [3:0]s,
+                          output cout);
+  assign {cout,s}=a+b+cin;
+endmodule
