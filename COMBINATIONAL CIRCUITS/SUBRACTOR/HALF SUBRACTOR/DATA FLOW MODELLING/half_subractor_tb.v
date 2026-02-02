efa0dba6918d3f38e6738ceeae32fee77cbaeeba
KOUSHIK C M <koushikmanohar83@gmail.com>
2026-02-02 13:18:38 +0530

    Create half_subractor_tb.v

---
+++ COMBINATIONAL CIRCUITS/SUBRACTOR/HALF SUBRACTOR/DATA FLOW MODELLING/half_subractor_tb.v
@@ -0,0 +1,21 @@
+module half_subractor_tb();
+  reg a,b;
+  wire d,br;
+  half_subractor dut(.a(a),.b(b),.d(d),.br(br));
+  initial begin
+    a=0;b=0;
+    #10 a=0;b=1;
+    #10 a=1;b=0;
+    #10 a=1;b=1;
+    #10 $finish;
+  end
+  initial begin
+    $monitor(" time=%0t  ||A=%b B=%b ||Difference=%b Borrow=%b",$time,a,b,d,br);
+  end
+  initial begin
+   $dumpfile("half_subractor.vcd");
+    $dumpvars;
+    #50
+    $finish;
+  end
+endmodule
