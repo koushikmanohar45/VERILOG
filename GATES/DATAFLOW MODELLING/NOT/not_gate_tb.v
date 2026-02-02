1805b1b32361a8b11b0cba5e052cb9875c542008
KOUSHIK C M <koushikmanohar83@gmail.com>
2026-02-02 14:40:19 +0530

    Create not_gate_tb.v

---
+++ GATES/DATAFLOW MODELLING/NOT/not_gate_tb.v
@@ -0,0 +1,19 @@
+module not_gate_tb ();
+  reg a;
+  wire y;
+  not_gate dut(.a(a),.y(y));
+  initial begin
+    a=0;
+    #2 a=1;
+    #2 $finish;
+  end
+  initial begin
+    $monitor(" time=%0t  ||A=%b   ||Y=%b",$time,a,y);
+  end
+  initial begin
+    $dumpfile("not_gate.vcd");
+    $dumpvars(1,not_gate_tb);
+    #10
+    $finish;
+  end
+endmodule
