03b984990f859469136027c1cbe3c448cec8ce75
KOUSHIK C M <koushikmanohar83@gmail.com>
2026-02-03 17:10:43 +0530

    Create mux4to1_using_mux2to1_tb.v

---
+++ COMBINATIONAL CIRCUITS/MULTIPLEXER/STRUCTURAL MODELLING/mux4to1_using_mux2to1_tb.v
@@ -0,0 +1,17 @@
+module mux4to1_using_mux2to1_tb();
+ reg i0,i1,i2,i3,s0,s1;
+ wire y;
+  mux4to1_using_mux2to1 dut(.i0(i0),.i1(i1),.i2(i2),.i3(i3),.s0(s0),.s1(s1),.y(y));
+  initial begin 
+    i0=0;i1=1;i2=0;i3=1;s0=0;s1=0;
+    $monitor("TIME=%0t  || IO=%B I1=%B I2=%B I4=%B  || S0=%B S1=%B  ||  Y=%B",$time,i0,i1,i2,i3,s0,s1,y);
+    #10 s0=1;s1=0;
+    #10 s0=0;s1=1;
+    #10 s0=1;s1=1;
+    #10 $finish;
+  end
+  initial begin
+    $dumpfile("mux4to1_using_mux2to1.vcd");
+    $dumpvars(1,mux4to1_using_mux2to1_tb);
+  end
+endmodule
