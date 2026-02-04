909d498c8fa945f63c2e3267936eec2d8b3687dc
KOUSHIK C M <koushikmanohar83@gmail.com>
2026-02-04 11:08:36 +0530

    Update mux4to1.v

--- COMBINATIONAL CIRCUITS/MULTIPLEXER/DATA FLOW MODELLING/4x1 MUX/mux4to1.v
+++ COMBINATIONAL CIRCUITS/MULTIPLEXER/DATA FLOW MODELLING/4x1 MUX/mux4to1.v
@@ -1,3 +1,6 @@
-module mux4to1(input i0,i1,i2,i3,s0,s1,output y);
-  assign y=s1?(s0?i3:i2):(s0?i1:i0);
+module mux4to1(input i0,i1,i2,i3,s0,s1,en,output y);
+  wire w1;
+  assign w1=s1?(s0?i3:i2):(s0?i1:i0);
+  assign y= en & w1;
+  
 endmodule 
