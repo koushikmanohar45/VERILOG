54e5113a2ed24a489fe496fc6d7611b966963c84
KOUSHIK C M <koushikmanohar83@gmail.com>
2026-02-04 12:25:35 +0530

    Create mux16x1_tb.v

---
+++ COMBINATIONAL CIRCUITS/MULTIPLEXER/DATA FLOW MODELLING/16x1 MUX/mux16x1_tb.v
@@ -0,0 +1,32 @@
+module mux16x1_tb();
+    reg [15:0]i;
+    reg [3:0]s;
+    reg en;
+    wire y;
+
+mux16x1 dut(.i(i),.s(s),.en(en),.y(y));
+    integer k;
+    initial begin
+      $monitor("en=%b s[3]=%b s[2]=%b s[1]=%b s[0]=%b || I=%b || y=%b",en, s[3],s[2],s[1],s[0],i, y);
+    end
+    initial begin
+        en=1;
+        i=16'b0000000011111111;
+        for (k =0;k<16;k=k+1) begin
+            s=k;
+            #10;
+            $display("en=%b s[3]=%b s[2]=%b s[1]=%b s[0]=%b || I=%b || y=%b",en, s[3],s[2],s[1],s[0],i, y);
+        end
+
+        en=0;
+        s =4'd5;
+        #10;
+        $display("en=%b s[3]=%b s[2]=%b s[1]=%b s[0]=%b || I=%b || y=%b",en, s[3],s[2],s[1],s[0],i, y);
+        $finish;
+    end
+    initial begin
+      $dumpfile("mux16x1.vcd");
+      $dumpvars(0,mux16x1_tb);
+    end
+
+endmodule
