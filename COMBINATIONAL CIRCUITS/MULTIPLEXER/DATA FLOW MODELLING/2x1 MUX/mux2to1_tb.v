8099a351ae1d8b0b8cba0acf71d578f1a238dda5
KOUSHIK C M <koushikmanohar83@gmail.com>
2026-02-04 11:06:43 +0530

    Update mux2to1_tb.v

--- COMBINATIONAL CIRCUITS/MULTIPLEXER/DATA FLOW MODELLING/2x1 MUX/mux2to1_tb.v
+++ COMBINATIONAL CIRCUITS/MULTIPLEXER/DATA FLOW MODELLING/2x1 MUX/mux2to1_tb.v
@@ -1,11 +1,12 @@
 module mux2to1_tb();
-  reg I0,I1,s;
+  reg I0,I1,s,en;
   wire y;
-  mux2to1 dut(.i0(I0),.i1(I1),.s(s),.y(y));
+  mux2to1 dut(.i0(I0),.i1(I1),.s(s),.en(en),.y(y));
   initial begin 
-    I0=1; I1=0; s=0;
-    $monitor("Time=%0t || I0=%b I1=%b  || S=%b || Y=%b",$time,I0,I1,s,y);
+    I0=1; I1=0; s=0;en=1;
+    $monitor("Time=%0t || I0=%b I1=%b  || en =%b S=%b || Y=%b",$time,I0,I1,s,en,y);
     #10 s=1;
+    #10 en=0;
     #10 $finish;
   end
   initial begin
