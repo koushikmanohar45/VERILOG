7a7a0aa43b759841a0629d73d436951fa7c6ede2
KOUSHIK C M <koushikmanohar83@gmail.com>
2026-02-04 11:18:54 +0530

    Update mux8to1.v

--- COMBINATIONAL CIRCUITS/MULTIPLEXER/DATA FLOW MODELLING/8x1 MUX/mux8to1.v
+++ COMBINATIONAL CIRCUITS/MULTIPLEXER/DATA FLOW MODELLING/8x1 MUX/mux8to1.v
@@ -1,3 +1,5 @@
-module mux8to1(input i0,i1,i2,i3,i4,i5,i6,i7,s0,s1,s2,output y);
-  assign y=s2?(s1?(s0?i7:i6):(s0?i5:i4)):(s1?(s0?i3:i2):(s0?i1:i0));
+module mux8to1(input i0,i1,i2,i3,i4,i5,i6,i7,s0,s1,s2,en,output y);
+  wire y1;
+  assign y1=s2?(s1?(s0?i7:i6):(s0?i5:i4)):(s1?(s0?i3:i2):(s0?i1:i0));
+  assign y= en&y1;
 endmodule 
