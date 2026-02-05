a6f5596d8eba8b83e072ffa8f1d568e27e6cc3f2
KOUSHIK C M <koushikmanohar83@gmail.com>
2026-02-05 10:59:10 +0530

    Update and rename demux4to1.v to demux1to4.v

--- COMBINATIONAL CIRCUITS/DEMULTIPLEXER/DATA FLOW MODELLING/1x4 DEMUX/demux1to4.v
+++ COMBINATIONAL CIRCUITS/DEMULTIPLEXER/DATA FLOW MODELLING/1x4 DEMUX/demux1to4.v
@@ -1,4 +1,4 @@
-module demux4to1(input y,s1,s0,output i0,i1,i2,i3);
+module demux1to4(input y,s1,s0,output i0,i1,i2,i3);
   assign i0=(~s1)&(~s0)&y;
   assign i1=(~s1)&(s0)&y;
   assign i2= (s1)&(~s0)&y;
