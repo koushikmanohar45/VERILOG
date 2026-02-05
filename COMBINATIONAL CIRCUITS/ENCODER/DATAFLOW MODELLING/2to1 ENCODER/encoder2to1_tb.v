c6484d464bd424820c6875e8c9725b58abb8a132
KOUSHIK C M <koushikmanohar83@gmail.com>
2026-02-05 13:07:57 +0530

    Create encoder2to1_tb.v

---
+++ COMBINATIONAL CIRCUITS/ENCODER/DATAFLOW MODELLING/2to1 ENCODER/encoder2to1_tb.v
@@ -0,0 +1,16 @@
+module encoder2to1_tb();
+  reg [1:0]D;
+  wire y;
+  encoder2to1 dut(.D(D),.y(y));
+  initial begin
+    $monitor("TIME=%0t  || D[1]=%b  D[0]=%b  ||y=%b",$time,D[1],D[0],y);           D=2'b00;
+    #10 D=2'b01;
+    #10 D=2'b10;
+    #10 D=2'b11;
+    #10 $finish;
+  end
+  initial begin
+    $dumpfile("encoder2to1.vcd");
+    $dumpvars(0,encoder2to1_tb);
+  end
+endmodule
