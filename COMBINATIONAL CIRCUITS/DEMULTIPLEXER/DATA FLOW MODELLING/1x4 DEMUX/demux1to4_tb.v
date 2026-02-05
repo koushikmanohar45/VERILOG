81498944892d47ab00b6f4889c7c75715e2d7db6
KOUSHIK C M <koushikmanohar83@gmail.com>
2026-02-05 11:00:09 +0530

    Update and rename demux4to1_tb.v to demux1to4_tb.v

--- COMBINATIONAL CIRCUITS/DEMULTIPLEXER/DATA FLOW MODELLING/1x4 DEMUX/demux1to4_tb.v
+++ COMBINATIONAL CIRCUITS/DEMULTIPLEXER/DATA FLOW MODELLING/1x4 DEMUX/demux1to4_tb.v
@@ -1,7 +1,7 @@
-module demux4to1_tb();
+module demux1to4_tb();
  reg y,s1,s0;
  wire i0,i1,i2,i3;
-  demux4to1 dut(.i0(i0),.i1(i1),.i2(i2),.i3(i3),.s1(s1),.s0(s0),.y(y));
+  demux1to4 dut(.i0(i0),.i1(i1),.i2(i2),.i3(i3),.s1(s1),.s0(s0),.y(y));
   initial begin 
     y=0;s1=0;s0=0;
     $monitor("TIME=%0t  || Y=%B  || S1=%B S0=%B   || IO=%B I1=%B I2=%B I4=%B",$time,y,s1,s0,i0,i1,i2,i3);
@@ -16,7 +16,7 @@ module demux4to1_tb();
     #10 $finish;
   end
   initial begin
-    $dumpfile("demux4to1.vcd");
-    $dumpvars(0,demux4to1_tb);
+   $dumpfile("demux1to4.vcd");
+   $dumpvars(0,demux1to4_tb);
   end
 endmodule
