1740b04b6e8ca46c2217ed01ddaafb986217cd7a
KOUSHIK C M <koushikmanohar83@gmail.com>
2026-02-05 19:48:00 +0530

    Update binary_to_gray_using_gates.v

--- COMBINATIONAL CIRCUITS/CODE CONVERTERS/BINARY TO GRAY/STRUCTURAL MODELLING/binary_to_gray_using_gates.v
+++ COMBINATIONAL CIRCUITS/CODE CONVERTERS/BINARY TO GRAY/STRUCTURAL MODELLING/binary_to_gray_using_gates.v
@@ -1,4 +1,4 @@
-module binary_to_gray_using_gates(input  [3:0] b,output [3:0] );
+module binary_to_gray_using_gates(input  [3:0] b,output [3:0]g );
   buf  (g[3], b[3]);
   xor  (g[2], b[3], b[2]);
   xor  (g[1], b[2], b[1]);
