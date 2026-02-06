edee8468d04963a16601f982d1ef210202abb8d2
KOUSHIK C M <koushikmanohar83@gmail.com>
2026-02-06 18:22:29 +0530

    Update jk_ff_sync_activelow_rst.v

--- SEQUENTIAL CIRCUITS/FLIPFLOP/NEGEDGE FLIPFLOP/JK-FLIPFLOP/jk_ff_sync_activelow_rst.v
+++ SEQUENTIAL CIRCUITS/FLIPFLOP/NEGEDGE FLIPFLOP/JK-FLIPFLOP/jk_ff_sync_activelow_rst.v
@@ -1,1 +1,20 @@
-M
+module jk_ff (input j,k,clk,rst,output reg q);
+  always @(negedge clk ) begin
+    if (!rst)
+        q <= 1'b0;
+    else begin
+      if(j)begin
+        if(k)
+          q<=~q;
+        else
+          q<=1'b1;
+      end
+      else begin
+        if(k)
+          q<=1'b0;
+        else
+          q<=q;
+      end
+     end
+  end
+endmodule
