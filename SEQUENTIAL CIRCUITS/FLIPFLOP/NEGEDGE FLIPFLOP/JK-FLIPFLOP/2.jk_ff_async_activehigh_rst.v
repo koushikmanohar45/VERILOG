11c7cb9129e6d77c293b998b8f77519b6be4908b
KOUSHIK C M <koushikmanohar83@gmail.com>
2026-02-06 18:29:59 +0530

    Create 2.jk_ff_async_activehigh_rst.v

---
+++ SEQUENTIAL CIRCUITS/FLIPFLOP/NEGEDGE FLIPFLOP/JK-FLIPFLOP/2.jk_ff_async_activehigh_rst.v
@@ -0,0 +1,20 @@
+module jk_ff (input j,k,clk,rst,output reg q);
+  always @(negedge clk or posedge rst) begin
+    if (rst)
+        q <= 1'b0;
+    else begin
+      if(j)begin
+        if(k)
+          q<=~q;
+        else
+          q<=1'b1;
+      end
+      else begin
+        if(k)
+          q<=1'b0;
+        else
+          q<=q;
+      end
+     end
+  end
+endmodule
