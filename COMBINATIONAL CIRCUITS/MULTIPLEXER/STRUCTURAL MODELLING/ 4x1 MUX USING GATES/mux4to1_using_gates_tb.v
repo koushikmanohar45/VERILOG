49bc2530222c6bf1415148d4449ca443aed36692
KOUSHIK C M <koushikmanohar83@gmail.com>
2026-02-06 16:42:42 +0530

    Update and rename  mux4to1_using_mux2to1_tb.v to mux4to1_using_gates_tb.v

--- COMBINATIONAL CIRCUITS/MULTIPLEXER/STRUCTURAL MODELLING/ 4x1 MUX USING GATES/mux4to1_using_gates_tb.v
+++ COMBINATIONAL CIRCUITS/MULTIPLEXER/STRUCTURAL MODELLING/ 4x1 MUX USING GATES/mux4to1_using_gates_tb.v
@@ -1,7 +1,7 @@
-module mux4to1_using_mux2to1_tb();
+module mux4to1_using_gates_tb();
  reg i0,i1,i2,i3,s0,s1;
  wire y;
-  mux4to1_using_mux2to1 dut(.i0(i0),.i1(i1),.i2(i2),.i3(i3),.s0(s0),.s1(s1),.y(y));
+  mux4to1_using_gates dut(.i0(i0),.i1(i1),.i2(i2),.i3(i3),.s0(s0),.s1(s1),.y(y));
   initial begin 
     i0=0;i1=1;i2=0;i3=1;s0=0;s1=0;
     $monitor("TIME=%0t  || IO=%B I1=%B I2=%B I4=%B  || S0=%B S1=%B  ||  Y=%B",$time,i0,i1,i2,i3,s0,s1,y);
@@ -11,7 +11,7 @@ module mux4to1_using_mux2to1_tb();
     #10 $finish;
   end
   initial begin
-    $dumpfile("mux4to1_using_mux2to1.vcd");
-    $dumpvars(1,mux4to1_using_mux2to1_tb);
+   $dumpfile("mux4to1_using_gates.vcd");
+   $dumpvars(1,mux4to1_using_gates_tb);
   end
 endmodule
