51104ff8dbadc1a00335ce39d586eb57526997de
KOUSHIK C M <koushikmanohar83@gmail.com>
2026-02-08 00:12:16 +0530

    Create alf_adder_using_if.v

---
+++ COMBINATIONAL CIRCUITS/ADDER/HALF ADDER/BEHAVIOUR MODELLING/using if/alf_adder_using_if.v
@@ -0,0 +1,16 @@
+module half_adder_using_if (input  a, b,output reg sum, carry);
+always @(*) begin
+    if (a==0 && b==0) begin
+        sum=0;carry=0;
+    end
+    else if (a==0 && b==1) begin
+        sum=1;carry=0;
+    end
+    else if(a==1 && b==0) begin
+        sum=1;carry=0;
+    end
+    else begin
+        sum=0;carry=1;
+    end
+end
+endmodule
