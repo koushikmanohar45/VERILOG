3f5f3ac769ed419494f65eb31173a5311b9cfaeb
KOUSHIK C M <koushikmanohar83@gmail.com>
2026-02-07 23:59:46 +0530

    Create half_adder_using_case_tb.v

---
+++ COMBINATIONAL CIRCUITS/ADDER/HALF ADDER/BEHAVIOUR MODELLING/using case/module half_adder_tb;    reg a, b;   wire sum, carry;    /half_adder_using_case_tb.v
@@ -0,0 +1,27 @@
+module half_adder_tb;
+
+  reg a, b;
+  wire sum, carry;
+
+  // Instantiate DUT
+  half_adder_using_case dut (
+    .a(a),
+    .b(b),
+    .sum(sum),
+    .carry(carry)
+  );
+
+  initial begin
+    $monitor("T=%0t a=%b b=%b | sum=%b carry=%b",
+              $time, a, b, sum, carry);
+
+    a = 0; b = 0;
+    #10 a = 0; b = 1;
+    #10 a = 1; b = 0;
+    #10 a = 1; b = 1;
+    #10 $finish;
+  end
+
+  initial begin
+    $dumpfile("half_adder.vcd");
+    $dumpvars(1, half_ad_
