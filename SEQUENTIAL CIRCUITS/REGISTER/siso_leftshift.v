a522aafa5b517bebda559fbdbedb703f5e4f59d1
KOUSHIK C M <koushikmanohar83@gmail.com>
2026-02-10 11:26:33 +0530

    Create siso_leftshift.v

---
+++ SEQUENTIAL CIRCUITS/REGISTER/siso_leftshift.v
@@ -0,0 +1,10 @@
+module sipo_dff#(parameter n=4)( input d,clk,rst,preset,output reg [n-1:0]q);
+  always @(posedge clk) begin
+    if(rst)
+      q<={n{1'b0}};
+    else if(preset)
+      q<={n{1'b1}};
+    else
+      q<={q[n-2:0],d};
+  end  
+endmodule
