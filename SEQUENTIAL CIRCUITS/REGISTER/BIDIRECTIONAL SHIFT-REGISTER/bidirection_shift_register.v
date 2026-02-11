2b6df71a45b5d7e5843d834330b144694e7c51e6
KOUSHIK C M <koushikmanohar83@gmail.com>
2026-02-11 15:34:33 +0530

    Update and rename bidirection_shift_register.v to bidirection_shift_register.v

--- SEQUENTIAL CIRCUITS/REGISTER/BIDIRECTIONAL SHIFT-REGISTER/bidirection_shift_register.v
+++ SEQUENTIAL CIRCUITS/REGISTER/BIDIRECTIONAL SHIFT-REGISTER/bidirection_shift_register.v
@@ -1,4 +1,4 @@
-module bidirection_shiftregister#(parameter n=4)(input sin,clk, rst,preset,input[1:0] sel,output reg [n-1:0]Q);
+module bidirection_shiftregister#(parameter n=4)(input sin_l,sin_r,clk,rst,preset, input[1:0] sel,output reg [n-1:0]Q);
   always @(posedge clk) begin
     if(rst)
       Q<={n{1'b0}};
@@ -6,9 +6,9 @@ module bidirection_shiftregister#(parameter n=4)(input sin,clk, rst,preset,input
       Q<={n{1'b1}};
     else begin
       if(sel==2'b10)
-        Q<={sin,Q[n-1:1]};//sel=10 rightshift
+        Q<={sin_r,Q[n-1:1]};//sel=10 right shift
       else if(sel==2'b01)
-        Q<={Q[n-2:0],sin};//se=01 left shift
+        Q<={Q[n-2:0],sin_l};//se=01 left shift
       else
         Q<=Q;//hold
     end
