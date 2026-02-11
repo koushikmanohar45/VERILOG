9f1a28763c784e74a56dbe2e3f50277a95a8a674
KOUSHIK C M <koushikmanohar83@gmail.com>
2026-02-11 18:14:29 +0530

    Create activelow_preset_and_rst_downcounter_tb.v

---
+++ SEQUENTIAL CIRCUITS/COUNTER/ASYNCHRONOUS/POSEDGE CLK/activelow_preset_and_rst_downcounter_tb.v
@@ -0,0 +1,18 @@
+module  asynchronous_negedge_clk_upcounter_tb();
+  reg clk,rst,preset;
+  wire [3:0]q;
+  asynchronous_negedge_clk_upcounter dut(.*);
+  always #5 clk=~clk;
+  initial begin
+    $monitor("TIME=%0t || clk=%b rst=%b preset=%b ||Q=%B",$time,clk,rst,preset,q);
+    clk=1;rst=0;preset=1;
+    #10 rst=1;
+    #155 preset=0;
+    #10 preset=1;
+    #200 $finish; 
+  end
+  initial begin
+    $dumpfile("asynchronous_negedge_clk_upcounter.vcd");
+    $dumpvars(1,asynchronous_negedge_clk_upcounter_tb);
+  end 
+endmodule 
