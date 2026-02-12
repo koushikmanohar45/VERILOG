24bdf5bcd8459b8984e2687923cf7600d79ac6f3
KOUSHIK C M <koushikmanohar83@gmail.com>
2026-02-12 18:31:05 +0530

    Create piso_right_shift_register_structural.v

---
+++ SEQUENTIAL CIRCUITS/REGISTER/PISO/right shift/piso_right_shift_register_structural.v
@@ -0,0 +1,20 @@
+module dff (input clk,rst,preset,d,output reg q);
+always @(posedge clk) begin
+    if (rst)
+        q<=1'b0;
+    else if(preset)
+        q<=1'b1;
+    else
+        q<=d;
+end
+endmodule
+
+module piso_dff(input [3:0]pin,input clk,rst,preset,sel,output sout);
+  wire [3:0]q;
+  
+  dff q0(clk,rst,preset,sel?pin[0]:1'b0,q[0]);//sel=0 shift s=1 parallel load 
+  dff q1(clk,rst,preset,sel?pin[1]:q[0],q[1]);
+  dff q2(clk,rst,preset,sel?pin[2]:q[1],q[2]);
+  dff q3(clk,rst,preset,sel?pin[3]:q[2],q[3]);
+  assign sout=q[3];
+endmodule
