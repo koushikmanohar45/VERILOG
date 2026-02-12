258ca201d8ffeaaf0a3d62b17edbd229499fcbcb
KOUSHIK C M <koushikmanohar83@gmail.com>
2026-02-12 18:31:28 +0530

    Create piso_right_shift_register_structural_tb.v

---
+++ SEQUENTIAL CIRCUITS/REGISTER/PISO/right shift/piso_right_shift_register_structural_tb.v
@@ -0,0 +1,28 @@
+module piso_dff_tb();
+  reg[3:0]pin;
+  reg clk,rst,preset,sel;
+  wire sout; 
+  piso_dff dut (.*);
+    initial begin
+      $monitor("TIME=%0t  || CLK= %B  RST=%B PRESET=%B SEL=%B || Pin=%B   ||Sout=%b",$time,clk,rst,preset,,sel,pin,sout);
+        rst=1;preset=0;sel=1;pin=4'b0001;
+        #40 rst=0;
+        #10 sel=0;
+        #60 sel=1;pin=4'b1100;
+        #40 sel=0;
+        #40 pin=4'b1110;
+        #40 sel=1;
+        #40 sel=0;pin=4'b0111;
+        #40 preset=1;
+        #40 $finish;
+    end
+  initial begin
+     clk=1;
+    forever #5 clk=~clk;
+  end
+  initial begin
+    $dumpfile("piso_dff.vcd");
+    $dumpvars(1,piso_dff_tb);
+  end
+
+endmodule
