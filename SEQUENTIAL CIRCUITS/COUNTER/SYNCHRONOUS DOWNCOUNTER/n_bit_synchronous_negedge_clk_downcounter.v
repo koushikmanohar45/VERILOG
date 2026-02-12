01a57c39a1f2ad4e46d63dc2ff199893e448c597
KOUSHIK C M <koushikmanohar83@gmail.com>
2026-02-12 12:42:53 +0530

    Create n_bit_synchronous_negedge_clk_downcounter.v

---
+++ SEQUENTIAL CIRCUITS/COUNTER/SYNCHRONOUS DOWNCOUNTER/n_bit_synchronous_negedge_clk_downcounter.v
@@ -0,0 +1,17 @@
+module synchronous_negedge_clk_downcounter_tb#(parameter n=5);
+  reg clk,rst;
+  wire [n-1:0] count;
+  synchronous_negedge_clk_downcounter dut(.*);
+  always# 5 clk=~clk;
+  initial begin
+    $dumpfile("synchronous_negedge_clk_downcounter.vcd");
+    $dumpvars(1,synchronous_negedge_clk_downcounter_tb);
+    $monitor("TIME=%0T ||CLK=%B RST=%B || COUNT=%B",$time,clk,rst,count);
+     clk=1;
+    rst=1;
+    #20 rst=0; 
+    #400 rst=1;
+    #30 rst=0;
+    #300 $finish;
+  end
+endmodule
