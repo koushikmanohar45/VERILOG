25a9b9e3cc3ce4fe34599db53d724787fc2e6606
KOUSHIK C M <koushikmanohar83@gmail.com>
2026-02-14 21:52:46 +0530

    Update and rename full_subractor.v to Half_subractor.v

---
+++ COMBINATIONAL CIRCUITS/SUBRACTOR/HALF SUBRACTOR/BEHAVIOR MODELLING/using operator/Half_subractor.v
@@ -0,0 +1,7 @@
+```
+module half_subractor(input a,b,output reg d,br);
+always@(*)begin
+{br,d}=a-b;
+end
+endmodule
+```
--- COMBINATIONAL CIRCUITS/SUBRACTOR/HALF SUBRACTOR/BEHAVIOR MODELLING/using operator/full_subractor.v
+++
@@ -1,6 +0,0 @@
-module full_subractor(input a,b,bin,output reg d,br);
-always@(*)begin
-{br,d}=a-b-bin;
-end
-endmodule
-
