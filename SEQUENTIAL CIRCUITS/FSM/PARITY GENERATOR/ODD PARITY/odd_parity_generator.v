4fd6ce0df9fcd149aaa99bbf75550d2cfdc7b877
KOUSHIK C M <koushikmanohar83@gmail.com>
2026-02-18 10:48:39 +0530

    Create odd_parity_generator.v

---
+++ SEQUENTIAL CIRCUITS/FSM/PARITY GENERATOR/ODD PARITY/odd_parity_generator.v
@@ -0,0 +1,22 @@
+module parity_generator(input clk,rst,input [2:0]x,output reg y);
+  localparam  s0=1'b0,s1=1'b1;
+  reg state,nxt_state;
+  always@(posedge clk or posedge rst) begin
+    if (rst)
+      state<=s0;
+    else
+      state<=nxt_state;
+  end
+  always @(*)begin
+    nxt_state=x[2]?(x[1]?(x[0]?s1:s0):(x[0]?s0:s1)):(x[1]?(x[0]?s0:s1):(x[0]?s1:s0));    
+  end
+  always@(*) begin
+      case(state)
+      s0:
+        y=1;
+      s1:
+        y=0;
+      default:y=0;
+    endcase
+    end
+endmodule
