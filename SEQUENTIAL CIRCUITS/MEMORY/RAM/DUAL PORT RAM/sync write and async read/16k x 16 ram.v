027121eae841cb02867e14e9f4e9b10a8cafe7cf
KOUSHIK C M <koushikmanohar83@gmail.com>
2026-02-19 13:11:33 +0530

    Update 16k x 16 ram.v

--- SEQUENTIAL CIRCUITS/MEMORY/RAM/DUAL PORT RAM/sync write and async read/16k x 16 ram.v
+++ SEQUENTIAL CIRCUITS/MEMORY/RAM/DUAL PORT RAM/sync write and async read/16k x 16 ram.v
@@ -1,4 +1,4 @@
-module dual_port_ram(
+  module dual_port_ram(
   input [15:0] data1,data2,
   input [13:0] addr1,addr2,
   input w_e1,w_e2,clk,
@@ -7,14 +7,16 @@ module dual_port_ram(
   reg [15:0] mem[16383:0];
   reg [13:0] addr_reg1;
   reg [13:0] addr_reg2;
-  always @(posedge clk) begin
+    always @(posedge clk ) begin
+    addr_reg1<=addr1;
     if(w_e1)
       mem[addr1]<=data1;
     else
       addr_reg1<=addr1;
   end
   always @(posedge clk) begin
-    if(w_e2 && !(w_e1 && addr1==addr2)) begin  //PORT1 is given PRIORITY   
+    addr_reg2<=addr2;
+    if(w_e2 && !(w_e1 && addr1==addr2)) begin  //PORT1 is given PRIORITY
       mem[addr2]<=data2;
     end
     else
@@ -22,4 +24,4 @@ module dual_port_ram(
   end     
   assign q1=mem[addr_reg1];
   assign q2=mem[addr_reg2];
-endmodule 
+endmodule
