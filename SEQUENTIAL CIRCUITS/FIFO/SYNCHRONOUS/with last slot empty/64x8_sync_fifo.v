4d48ac34927e0845841df4a6fa122b2438c9081a
KOUSHIK C M <koushikmanohar83@gmail.com>
2026-02-20 11:32:52 +0530

    Create 64x8_sync_fifo.v

---
+++ SEQUENTIAL CIRCUITS/FIFO/SYNCHRONOUS/with last slot empty/64x8_sync_fifo.v
@@ -0,0 +1,39 @@
+module sync_fifo(
+  input[7:0] data_in,
+  input rd_en,wt_en,clk,rst,
+  output reg [7:0] data_out
+);
+  reg[7:0]mem[63:0];
+  reg [5:0] wptr,rptr;
+  wire full,empty;
+  always@(posedge clk) begin
+    if(rst) begin
+      wptr<=6'd0;
+      data_out<=8'd0;
+    end
+   else begin
+     if(wt_en && !full) begin 
+           mem[wptr]<=data_in;
+           wptr<=wptr+1;
+       end
+     else
+         wptr<=wptr;
+      end
+   end
+
+ always @(posedge clk) begin
+  if(rst)
+     rptr<=6'd0;       
+  else begin 
+    if(rd_en && !empty) begin
+         data_out<=mem[rptr];
+         rptr<=rptr+1;
+        end
+       else
+         rptr<=rptr;
+      end
+   end
+
+  assign empty=(wptr==rptr)?1'b1:1'b0;
+  assign full=(rptr==(wptr+1'b1))?1'b1:1'b0;
+endmodule
