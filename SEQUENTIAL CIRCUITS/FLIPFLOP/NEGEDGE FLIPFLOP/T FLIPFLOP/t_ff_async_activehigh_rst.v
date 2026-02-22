45c0a03cefda739c8416e57aa357b117b732eb14
KOUSHIK C M <koushikmanohar83@gmail.com>
2026-02-22 23:40:43 +0530

    Create t_ff_async_activehigh_rst.v

---
+++ SEQUENTIAL CIRCUITS/FLIPFLOP/NEGEDGE FLIPFLOP/T FLIPFLOP/t_ff_async_activehigh_rst.v
@@ -0,0 +1,12 @@
+module t_ff(input t,clk,rst,output reg q);
+ always@(negedge clk or posedge rst)begin
+  if(rst)
+    q<=1'b0;
+  else begin
+    if(t)
+      q<=~q;
+    else
+      q<=q;
+  end
+ end
+endmodule
