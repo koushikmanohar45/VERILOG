60d474fd7a359853cfeab0ad9c50894fd45b4667
KOUSHIK C M <koushikmanohar83@gmail.com>
2026-02-26 19:34:43 +0530

    Create t_to_sr_tb.v

---
+++ SEQUENTIAL CIRCUITS/FLIPFLOP CONVERSION/T to SR/t_to_sr_tb.v
@@ -0,0 +1,26 @@
+module t_to_sr_tb;
+    reg s,r,clk,rst;
+    wire q;
+  t_to_sr dut (.s(s),.r(r),.clk(clk),.rst(rst),.q(q));
+    initial begin
+      $monitor("TIME=%0t  || CLK= %B  RST=%B  || S=%B  R=%B  || Q= %B",$time,clk,rst,s,r,q);
+        clk=0;rst=1;s=0;r=1;
+        @(negedge clk) rst=0; 
+        @(negedge clk) s=1;r=0;
+        @(negedge clk) s=0;r=0;
+        @(negedge clk)   s=0;r=1;
+        @(negedge clk)   rst=0; 
+        @(negedge clk) s=1;r=1;
+        @(negedge clk) s=1;r=0;
+        @(negedge clk) $finish;
+    end
+  initial begin
+     clk=1;
+    forever #5 clk=~clk;
+  end
+  initial begin
+    $dumpfile("t_to_sr.vcd");
+    $dumpvars(1,t_to_sr_tb);
+  end
+
+endmodule
