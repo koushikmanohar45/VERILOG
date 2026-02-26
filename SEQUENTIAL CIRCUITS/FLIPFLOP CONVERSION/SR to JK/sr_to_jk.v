042c842c5f4642d27f7c5cc1959d0061724343aa
KOUSHIK C M <koushikmanohar83@gmail.com>
2026-02-26 19:29:18 +0530

    Create sr_to_jk.v

---
+++ SEQUENTIAL CIRCUITS/FLIPFLOP CONVERSION/SR to JK/sr_to_jk.v
@@ -0,0 +1,19 @@
+module sr_ff (input s,r,clk,rst,output reg q);
+  always @(posedge clk ) begin
+    if (rst)
+        q <= 1'b0;
+    else begin
+      case({s,r})
+        2'b00: q<=q;
+        2'b01: q<=1'b0;
+        2'b10: q<=1'b1;
+        2'b11: q<=1'bx;
+        default: q<=q;
+      endcase
+  end
+end
+endmodule
+
+module sr_to_jk(input j,k,clk,rst,output q);
+  sr_ff sr1(.s(~q&j),.r(q&k),.clk(clk),.rst(rst),.q(q));
+endmodule
