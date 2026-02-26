2e57b7ef75c1009051dedfd7dad10adf9be83fa3
KOUSHIK C M <koushikmanohar83@gmail.com>
2026-02-26 19:40:26 +0530

    Create jk_to_d_tb.v

---
+++ SEQUENTIAL CIRCUITS/FLIPFLOP CONVERSION/JK to D/jk_to_d_tb.v
@@ -0,0 +1,27 @@
+module jk_to_d_tb;
+    reg d,clk,rst;
+    wire q;
+    jk_to_d dut (.d(d),.clk(clk),.rst(rst),.q(q));
+    initial begin
+      $monitor("TIME=0%t  || CLK= %B  RST=%B  || D=%B  || Q= %B",$time,clk,rst,d,q);
+         clk=0;rst=1;d=0;
+         @(negedge clk) rst=0; 
+         @(negedge clk) d=1;
+         @(negedge clk) d=0;
+         @(negedge clk) d=1;
+         @(negedge clk) rst=1; 
+         @(negedge clk) d=0;
+         @(negedge clk) d=1;
+         @(negedge clk) rst=0;
+         @(negedge clk) $finish;
+    end
+  initial begin
+     clk=1;
+    forever #5 clk=~clk;
+  end
+  initial begin
+    $dumpfile("jk_to_d.vcd");
+    $dumpvars(0,jk_to_d_tb);
+  end
+
+endmodule
