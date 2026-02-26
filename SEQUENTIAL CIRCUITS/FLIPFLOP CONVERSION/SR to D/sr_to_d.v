4cdf47edda6768cf7c428e4b552cefb1553d295b
KOUSHIK C M <koushikmanohar83@gmail.com>
2026-02-26 19:41:25 +0530

    Create sr_to_d.v

---
+++ SEQUENTIAL CIRCUITS/FLIPFLOP CONVERSION/SR to D/sr_to_d.v
@@ -0,0 +1,18 @@
+module sr_ff (input s,r,clk,rst,output reg q);
+  always @(posedge clk ) begin
+    if (rst)
+        q <= 1'b0;
+    else begin
+      case({s,r})
+        2'b00: q<=q;
+        2'b01: q<=1'b0;
+        2'b10: q<=1'b1;
+        2'b11: q<=1'bx;
+        default: q<=q;
+      endcase
+  end
+end
+endmodule
+module sr_to_d(input d,clk,rst,output q);
+  sr_ff sr1(.s(d),.r(~d),.clk(clk),.rst(rst),.q(q));
+endmodule
