1ce934b0734677847b806bf944172460aeab721e
KOUSHIK C M <koushikmanohar83@gmail.com>
2026-02-27 13:21:28 +0530

    Create Q19.v

---
+++ ASSIGNMENTS/STRUCTURAL MODLLING/Q19.v
@@ -0,0 +1,21 @@
+//Expect the simulator output
+module tb_top;
+wire w1;
+xmit xmit_i1(w1);
+rcv rcv_i1 (w1);
+endmodule
+
+module xmit (output reg w);
+ initial w=1;
+endmodule
+
+module rcv (input wire w);
+initial begin
+$display ("w value is ");
+$display(w);
+end
+endmodule
+
+OUTPUT:
+w value is 
+1
