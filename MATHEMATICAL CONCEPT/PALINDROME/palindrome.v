8fd52d595a0eb2930a347b3f9ff3b890203f4e38
KOUSHIK C M <koushikmanohar83@gmail.com>
2026-02-28 17:53:27 +0530

    Create palindrome.v

---
+++ MATHEMATICAL CONCEPT/PALINDROME/palindrome.v
@@ -0,0 +1,24 @@
+module palindrome
+(
+  input  [15:0]n,
+  output reg out
+);
+  integer temp,reverse,digit;
+
+  always @(*) begin
+    temp=n;
+    reverse=0;
+
+    while (temp!=0) begin
+      digit=temp%10;
+      temp=temp/10;
+      reverse= reverse*10+digit;
+    end
+
+    if (reverse==n)
+      out=1;
+    else
+      out=0;
+  end
+
+endmodule
