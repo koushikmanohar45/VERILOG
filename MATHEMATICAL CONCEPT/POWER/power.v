7df37d595c838b0a0e2de0a6748dc9a526cc57dc
KOUSHIK C M <koushikmanohar83@gmail.com>
2026-02-28 12:18:48 +0530

    Update power.v

--- MATHEMATICAL CONCEPT/POWER/power.v
+++ MATHEMATICAL CONCEPT/POWER/power.v
@@ -1,14 +1,15 @@
-module a_power #(parameter N=4,pow=3)(input [N-1:0] data,output reg [2*N-1:0]out);
+module a_power #(parameter N=4,pow=3)(input [N-1:0] data,output reg [pow*N-1:0]out);
   integer i;
   always@(*) begin
     sq(data,out);
   end
   task automatic sq;
-    input [N:0] a;
-    output[2*N-1:0] out;
+    input [N-1:0] a;
+    output[pow*N-1:0] out;
     out=1;
     for(i=1;i<=pow;i++) begin
      out=out*a;
     end
   endtask
 endmodule
+  
