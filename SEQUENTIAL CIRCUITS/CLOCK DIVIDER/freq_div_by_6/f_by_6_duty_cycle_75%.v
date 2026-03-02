9809169a2c4a2395707fb3e7300847fd91a46e1b
KOUSHIK C M <koushikmanohar83@gmail.com>
2026-03-02 15:38:13 +0530

    Create f_by_6_duty_cycle_75%.v

---
+++ SEQUENTIAL CIRCUITS/CLOCK DIVIDER/freq_div_by_6/f_by_6_duty_cycle_75%.v
@@ -0,0 +1,33 @@
+module clk_divider(
+  input clk,rst,
+  output reg f_by_6
+);
+  reg[2:0] count;
+  always @(posedge clk) begin
+    if(rst) begin
+      count<=3'b000;
+      f_by_6<=1'b0;
+    end
+    else begin
+      if(count==3'b101) begin
+        f_by_6<=~f_by_6;
+        count<=3'b000;
+      end
+      else  
+        count<=count+1;
+        end
+      end
+  
+  
+  always @(negedge clk) begin
+    if(rst) begin
+      count<=3'b000;
+      f_by_6<=1'b1;
+    end
+    else begin
+    if(count==3'b001) 
+        f_by_6<=~f_by_6;
+      end
+    end
+  
+endmodule
