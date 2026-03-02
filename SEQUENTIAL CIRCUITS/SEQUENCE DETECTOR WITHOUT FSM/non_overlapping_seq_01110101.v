7103b2464b1441e0071910a18ca43a232bc06338
KOUSHIK C M <koushikmanohar83@gmail.com>
2026-03-02 18:44:30 +0530

    Create non_overlapping_seq_01110101.v

---
+++ SEQUENTIAL CIRCUITS/SEQUENCE DETECTOR WITHOUT FSM/non_overlapping_seq_01110101.v
@@ -0,0 +1,21 @@
+module seq_detector#(parameter seq=8'b01110101)(
+  input clk,rst,x,
+  output reg y
+);
+  reg[7:0] temp_register;
+  always@(posedge clk or posedge rst) begin
+    if(rst)begin
+       temp_register<=8'd0;
+       y<=1'b0;
+    end
+    else begin
+      temp_register<={temp_register[6:0],x};
+      if(temp_register==seq) begin
+        y<=1'b1;
+        temp_register<=8'd0;
+      end
+      else
+        y<=1'b0;
+    end
+  end     
+endmodule
